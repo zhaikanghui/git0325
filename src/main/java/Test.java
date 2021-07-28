f1f1febefa62c2dc763464659c23d59b645b2638
zkh <zkh@163.com>
2021-07-28 17:18:05 +0800

    fix

--- src/main/java/Test.java
+++ src/main/java/Test.java
@@ -3,6 +3,6 @@ public class Test {
         System.out.println("aaaaaaa");
         System.out.println("bbbbb");
         System.out.println("hot_fix");
-        System.out.println("master");
+        System.out.println("m");
     }
 }
